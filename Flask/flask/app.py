0b341beb00ab96a2ce146079d0168b3e93d98399
Mingkai880904 <43432006+Mingkai880904@users.noreply.github.com>
2021-09-28 20:19:10 +0800

    Add files via upload

--- Flask/flask/app.py
+++ Flask/flask/app.py
@@ -12,17 +12,37 @@ import numpy as np
 from keras.preprocessing.image import load_img
 
 @app.route('/')
-def index():
-	return render_template('index.html')
+def hello_world():
+    return render_template("index.html")
+database={'admin':'123','ksu':'ksu','cnn':'cnn'}
+
+@app.route('/web',methods=['POST','GET'])
+def login():
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+	    return render_template('index.html',info='Invalid User')
+    else:
+        if database[name1]!=pwd:
+            return render_template('index.html',info='Invalid Password')
+        else:
+	         return render_template('upload.html',name=name1)
+
+if __name__ == '__main__':
+    app.run()
+
+# @app.route('/')
+# def index():
+# 	return render_template('index.html')
 
 @app.route('/Sign_up', methods=['GET'])
 def signup():
         return render_template('Sign_up.html')
 
 
-@app.route('/web', methods=['GET'])
-def web():
-        return render_template('上傳.html')
+# @app.route('/web', methods=['GET'])
+# def web():
+#         return render_template('上傳.html')
 
 @app.route('/aboutteam', methods=['GET'])
 def team():
@@ -40,14 +60,14 @@ def web2():
     image_path = "./images/" + imagefile.filename
     imagefile.save(image_path)  
 
-    model = tensorflow.keras.models.load_model('keras_model.h5', compile=False)
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    model = tensorflow.keras.models.load_model('pneumonia_cnn0927.h5', compile=False)
+    data = np.ndarray(shape=(1, 200, 200, 1), dtype=np.float32)
 
     # Replace this with the path to your image
-    image = load_img(image_path,target_size=(224,224))
+    image = load_img(image_path,target_size=(200,200))
     #resize the image to a 224x224 with the same strategy as in TM2:
     #resizing the image to be at least 224x224 and then cropping from the center
-    size = (224, 224)
+    size = (200, 200)
     image = ImageOps.fit(image, size)
     image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
